9760d48ce44bbe0b7a51bac33679f64dcc62c490
mnbf9fca <robert.aleck@cynexia.com>
2019-10-19 08:01:21 +0100

    moved

---
+++ basic_config.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+# install authorized_keys
+#
+# FIRST, DOWNLOAD AUTHORIZED_KEYS
+# https://github.com/mnbf9rca/server-config/raw/master/authorized_keys
+#
+
+echo Installing keys
+cd ~
+mkdir ~/.ssh
+cp  ~/authorized_keys ~/.ssh/authorized_keys
+echo ... key saved
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+echo ... key secured
+
+# ensure cart auth is allowed authentication
+echo enabling cert auth
+sed -i 's|[#]*ChallengeResponseAuthentication yes|ChallengeResponseAuthentication no|g' /etc/ssh/sshd_config
+sed -i 's|[#]*PubkeyAuthentication no|PubkeyAuthentication yes|g' /etc/ssh/sshd_config
+echo ... reloading sshd
+systemctl reload sshd
+
+# install automatic updates
+echo enable auto updates
+echo ... installing
+apt-get update
+apt-get upgrade -y
+apt-get install -y unattended-upgrades apt-transport-https ca-certificates
+echo ... setting timing
+sed -i 's|^//[ \t]\"\${distro_id}:\${distro_codename}-updates\"|"\${distro_id}:\${distro_codename}-updates\"|g' /etc/apt/apt.conf.d/50unattended-upgrades
+sed -i 's|^//[ \t]*Unattended-Upgrade::Automatic-Reboot \"false\";|Unattended-Upgrade::Automatic-Reboot \"true\";|g' /etc/apt/apt.conf.d/50unattended-upgrades
+sed -i 's|^//[ \t]*Unattended-Upgrade::Remove-Unused-Dependencies \"false\";|Unattended-Upgrade::Remove-Unused-Dependencies   \"true\";|g' /etc/apt/apt.conf.d/50unattended-upgrades
+sed -i 's|^//[ \t]*Unattended-Upgrade::AutoFixInterruptedDpkg \"false\";|Unattended-Upgrade::AutoFixInterruptedDpkg \"true\";|g' /etc/apt/apt.conf.d/50unattended-upgrades
+cp 10periodic /etc/apt/apt.conf.d/10periodic
+echo ... automatic upgrades configured
+
+# install other useful apps
+echo installing other apps
+sudo apt-get install -y nano 
+export TERM=xterm
+
+echo ... done
