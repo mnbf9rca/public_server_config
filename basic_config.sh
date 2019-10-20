3bf12c2904b004f34a26e3d81c2b6b34afc54a76
Rob Aleck <mnbf9rca@users.noreply.github.com>
2019-10-20 10:03:20 +0100

    Update basic_config.sh

--- basic_config.sh
+++ basic_config.sh
@@ -25,12 +25,18 @@ sed -i 's|[#]*PubkeyAuthentication no|PubkeyAuthentication yes|g' /etc/ssh/sshd_
 echo ... reloading sshd
 systemctl reload sshd
 
+# install key packages, update system
+apt-get update
+apt-get dist-upgrade --autoremove --no-install-recommends --assume-yes
+# install other useful apps
+echo installing other apps
+apt-get install -y nano open-vm-tools
+
 # install automatic updates
 echo enable auto updates
 echo ... installing
 wget -O10periodic https://raw.githubusercontent.com/mnbf9rca/public_server_config/master/10periodic
-apt-get update
-apt-get dist-upgrade --autoremove --no-install-recommends --assume-yes
+
 apt-get install -y unattended-upgrades apt-transport-https ca-certificates
 echo ... setting timing
 sed -i 's|^//[ \t]\"\${distro_id}:\${distro_codename}-updates\"|"\${distro_id}:\${distro_codename}-updates\"|g' /etc/apt/apt.conf.d/50unattended-upgrades
@@ -39,10 +45,5 @@ sed -i 's|^//[ \t]*Unattended-Upgrade::Remove-Unused-Dependencies \"false\";|Una
 sed -i 's|^//[ \t]*Unattended-Upgrade::AutoFixInterruptedDpkg \"false\";|Unattended-Upgrade::AutoFixInterruptedDpkg \"true\";|g' /etc/apt/apt.conf.d/50unattended-upgrades
 cp 10periodic /etc/apt/apt.conf.d/10periodic
 echo ... automatic upgrades configured
-
-# install other useful apps
-echo installing other apps
-sudo apt-get install -y nano 
-export TERM=xterm
 
 echo ... done
