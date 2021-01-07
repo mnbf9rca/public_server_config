1c696e72e046b03a495a92decbad1dd66a983a62
rob aleck <robert.aleck@shell.com>
2021-01-07 10:16:59 +0000

    add password, switch to useradd

--- add_user.sh
+++ add_user.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
+usage() { echo "Usage: $0 -u <username> -p <password>" 1>&2; exit 1; }
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-while getopts u:a:f: flag
+while getopts u:p: flag
 do
     case "${flag}" in
         u) username=${OPTARG};;
+        p) password=${OPTARG};;
+        \?) usage
     esac
 done
-if [ -z "$username" ];
-then
-    echo "set -u flag"
-    exit 1
-else
-    echo "Username: $username";
-fi
+if [ -z "$username" ]; then usage fi
+if [ -z "$password" ]; then usage fi
+
 
 # create user
 echo "Creating user $username"
-adduser $username
+useradd --create-home --password $password $username
 retVal=$?
 [[ $retVal -ne 0 ]] && {echo "... operation failed, error code $retVal"; exit 1}
 
