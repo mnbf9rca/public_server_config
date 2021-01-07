6fd98bf9555c61fb3641091b58d58fbf6b9fd804
rob aleck <robert.aleck@shell.com>
2021-01-07 11:16:03 +0000

    .

--- add_user.sh
+++ add_user.sh
@@ -22,7 +22,6 @@ hashed = mak
 echo "Creating user $username"
 adduser --gecos "" --disabled-password $username
 retVal=$?
-echo "return {$retVal}"
 [[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
 
 echo "setting password"
@@ -30,5 +29,64 @@ echo "{$username}":"{$password}" | chpasswd -e
 retVal=$?
 echo "return {$retVal}"
 [[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+echo "Adding $username to sudo group"
+usermod -aG sudo $username
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+echo "Creating $username/.ssh"
+mkdir $username/.ssh
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+echo "downloading ssh keys"
+wget -O$username/.ssh/authorized_keys https://github.com/mnbf9rca.keys
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+echo "... key saved"
+echo "... chown"
+chown -R $username:$username $username/.ssh
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+echo "... chmod folder"
+chmod 700 $username/.ssh
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+echo "... chmod key"
+chmod 600 $username/.ssh/authorized_keys
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+echo "... key secured"
+echo enabling cert auth
+sed -i 's|[#]*ChallengeResponseAuthentication yes|ChallengeResponseAuthentication no|g' /etc/ssh/sshd_config
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+sed -i 's|[#]*PubkeyAuthentication no|PubkeyAuthentication yes|g' /etc/ssh/sshd_config
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+systemctl reload sshd
+retVal=$?
+echo "return {$retVal}"
+[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+
+
+
+
+
 
 echo ... done
