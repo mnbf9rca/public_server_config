7187a3c6afff555e9cf2178c0b5688ff15206dfe
rob aleck <robert.aleck@shell.com>
2021-01-07 12:16:29 +0000

    checkerror

--- add_user.sh
+++ add_user.sh
@@ -29,63 +29,43 @@ checkerror $?
 
 echo "setting password"
 echo "{$username}":"{$password}" | chpasswd -e
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 echo "Adding $username to sudo group"
 usermod -aG sudo $username
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 echo "Creating $username/.ssh"
 mkdir $username/.ssh
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 echo "downloading ssh keys"
 wget -O$username/.ssh/authorized_keys https://github.com/mnbf9rca.keys
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 echo "... key saved"
 echo "... chown"
 chown -R $username:$username $username/.ssh
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 echo "... chmod folder"
 chmod 700 $username/.ssh
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 echo "... chmod key"
 chmod 600 $username/.ssh/authorized_keys
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 echo "... key secured"
 echo enabling cert auth
 sed -i 's|[#]*ChallengeResponseAuthentication yes|ChallengeResponseAuthentication no|g' /etc/ssh/sshd_config
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 sed -i 's|[#]*PubkeyAuthentication no|PubkeyAuthentication yes|g' /etc/ssh/sshd_config
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 systemctl reload sshd
-retVal=$?
-echo "return {$retVal}"
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 
 
