5e60d6bbd81afbbfb33e2dc4de4c94382bb4567f
rob aleck <robert.aleck@shell.com>
2021-01-07 11:20:24 +0000

    checkerror

--- add_user.sh
+++ add_user.sh
@@ -2,6 +2,10 @@
 
 usage() { echo "Usage: $0 -u <username> -p <password>" 1>&2; exit 1; }
 
+function checkerror{
+   [ $1 -ne 0 ]] && { echo "... operation failed, error code {$1}"; exit 1 ; } 
+}
+
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 while getopts "u:p:" flag; do
@@ -21,8 +25,7 @@ hashed = mak
 # create user
 echo "Creating user $username"
 adduser --gecos "" --disabled-password $username
-retVal=$?
-[[ $retVal -ne 0 ]] && { echo "... operation failed, error code {$retVal}"; exit 1 ; }
+checkerror $?
 
 echo "setting password"
 echo "{$username}":"{$password}" | chpasswd -e
