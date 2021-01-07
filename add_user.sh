38a9fbc9622e1b4039632c9db200a52c7ed3aeda
rob aleck <robert.aleck@shell.com>
2021-01-07 10:21:23 +0000

    ;;

--- add_user.sh
+++ add_user.sh
@@ -4,17 +4,17 @@ usage() { echo "Usage: $0 -u <username> -p <password>" 1>&2; exit 1; }
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-while getopts u:p: flag
-do
+while getopts "u:p:" flag; do
     case "${flag}" in
         u) username=${OPTARG};;
         p) password=${OPTARG};;
-        \?) usage
+        *) usage ;;
     esac
 done
-if [ -z "$username" ]; then usage fi
-if [ -z "$password" ]; then usage fi
 
+if [ -z "${username}" ] || [ -z "${password}" ]; then
+    usage
+fi
 
 # create user
 echo "Creating user $username"
