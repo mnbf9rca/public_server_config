dac5b741cd62d4ca4ce5ebd6f02728ce8d4138b4
rob aleck <robert.aleck@shell.com>
2021-01-07 13:44:23 +0000

    tidy

--- update_and_clean.sh
+++ update_and_clean.sh
@@ -10,21 +10,17 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 # install key packages, update system
 echo "Updating catalogue..."
 apt update
-retVal=$?
 checkerror $?
 
 echo "Removing snap"
 apt purge -y snapd
-retVal=$?
 checkerror $?
 
 echo "updating the rest"
 apt dist-upgrade --autoremove --no-install-recommends --assume-yes
-retVal=$?
 checkerror $?
 
 # install other useful apps
 echo "installing other apps"
 apt install -y nano curl
-retVal=$?
 checkerror $?
