9c6a25f232e6ab8868b6412db4bd201186e02c60
Rob Aleck <mnbf9rca@users.noreply.github.com>
2022-11-15 22:05:38 +0000

    set email for automatic update notifications (and linting)

--- automatic_updates.sh
+++ automatic_updates.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
+# check if there is an argument
+if [ ! $# -eq 0 ]; then
+  # if so, check the email address is valid
+  if [[ $1 =~ [a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,} ]]; then
+    echo "Valid email address: $email_address"
+    $email_address = $1
+  else
+    echo "Email address $email_address is invalid - expect something like x@x.xx"
+    exit 1
+  fi
+fi
+
 function checkerror() {
-   
-   [[ $1 -ne 0 ]] && { echo "... operation failed, error code {$1}"; exit 1 ; } 
+  [[ $1 -ne 0 ]] && {
+    echo "... operation failed, error code {$1}"
+    exit 1
+  }
 }
 
 function backupfile() {
-   pathname=$1
-   file=${pathname##*/}
-   dt=$(date '+%Y%m%d-%H%M%S');
-   echo "File $pathname already exists! backing up to $HOME/$file-$dt"
-   cp -f -v $pathname $HOME/$file-$dt
-   checkerror $?
+  pathname=$1
+  file=${pathname##*/}
+  dt=$(date '+%Y%m%d-%H%M%S')
+  echo "File $pathname already exists! backing up to $HOME/$file-$dt"
+  cp -f -v $pathname $HOME/$file-$dt
+  checkerror $?
 }
 
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+if [[ $(id -u) -ne 0 ]]; then
+  echo "Please run as root"
+  exit 1
+fi
 
 # install automatic updates
 echo "enable auto updates"
@@ -29,15 +46,15 @@ checkerror $?
 echo ... creating schedule config file
 configfile="/etc/apt/apt.conf.d/10periodic"
 if [ -e $configfile ]; then
-   backupfile $configfile
+  backupfile $configfile
 else
-  cat > $configfile <<EOF
+  cat >$configfile <<EOF
 APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Download-Upgradeable-Packages "1";
 APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 EOF
-checkerror $?
+  checkerror $?
 fi
 
 echo ... setting timing
@@ -50,22 +67,31 @@ checkerror $?
 sed -i 's|^//[ \t]*Unattended-Upgrade::AutoFixInterruptedDpkg \"false\";|Unattended-Upgrade::AutoFixInterruptedDpkg \"true\";|g' /etc/apt/apt.conf.d/50unattended-upgrades
 checkerror $?
 
+# if $email_address is not empty then set it
+if [ -z "${email_address}" ]; then
+  echo ... setting email address
+  sed -i 's|^//[ \t]*Unattended-Upgrade::MailReport \"on-change\";|Unattended-Upgrade::MailReport \"always\";|g' /etc/apt/apt.conf.d/50unattended-upgrades
+  checkerror $?
+  sed -i "s|^//[ \t]*Unattended-Upgrade::Mail \"\";|Unattended-Upgrade::Mail \"$email_address\";|g" /etc/apt/apt.conf.d/50unattended-upgrades
+  checkerror $?
+fi
+
 echo ... enabling
 configfile="/etc/apt/apt.conf.d/20auto-upgrades"
 if [ -e $configfile ]; then
   backupfile $configfile
 else
-  cat > $configfile <<EOF
+  cat >$configfile <<EOF
 // from https://wiki.debian.org/UnattendedUpgrades
 // most config in 10periodic
 // Enable the update/upgrade script (0=disable)
 APT::Periodic::Enable "1";
 EOF
-checkerror $?
+  checkerror $?
 fi
 
 # check if it's all ok
-unattended-upgrade --dry-run --verbose
+unattended-upgrade --verbose
 checkerror $?
 
 echo ... automatic upgrades configured
