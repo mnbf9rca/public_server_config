5c7233e417f5d77fb044d8b35349da8d8adadde6
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-11-10 13:50:32 +0000

    Update backup_database.sh to use barman

--- backup_database.sh
+++ backup_database.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Variables
-# assumes database is called tsdb
-BACKUP_DIR="/backup"
-DATE_SUFFIX=$(date +%F)
-BACKUP_FILE="$BACKUP_DIR/tsdb_backup_$DATE_SUFFIX.sql"
-ZIP_FILE="$BACKUP_DIR/tsdb_backup_$DATE_SUFFIX.zip"
-LOG_FILE="$BACKUP_DIR/tsdb_backup_log_$DATE_SUFFIX.txt"
-S3_BUCKET="s3://pg.cynexia.net-backup"
-HEALTHCHECK_URL="https://hc-ping.com/1cf97622-ec87-486e-9cad-dd9598bef64a"
-PG_BACKUP_USER="backup_user"
-PG_PORT="31432"
-RETAIN_BACKUPS_AWS=7  # Number of backups to retain in AWS
-RETAIN_BACKUPS_LOCAL=1 # Number of backups to retain locally
+BACKUP_DIR="/var/lib/postgresql/backup"
+DATE_SUFFIX=$(date +%F_%H-%M-%S)
+LOG_FILE="$BACKUP_DIR/barman_backup_log_$DATE_SUFFIX.txt"
+S3_BUCKET="s3://<container_name>/barman"
+HEALTHCHECK_URL="https://hc-ping.com/<UUID>"
+SERVER_NAME="pg"
+RETENTION_POLICY="RECOVERY WINDOW OF 30 DAYS"  # Adjust the retention policy as needed
+RETAIN_LOG_DAYS=7
+
+# create backup temp dir if it doesnt exist
+mkdir -p $BACKUP_DIR
 
 # Redirect all output to log file
 exec > "$LOG_FILE" 2>&1
@@ -23,34 +22,20 @@ send_log() {
     curl -fsS --retry 3 -m 10 -X POST -H "Content-Type: text/plain" --data-binary "@$LOG_FILE" "$url"
 }
 
-# back up database
-pg_dump -U $PG_BACKUP_USER -h localhost -p $PG_PORT tsdb -f "$BACKUP_FILE" || {
+# Perform backup with Barman
+barman-cloud-backup -v --cloud-provider aws-s3 --snappy -d postgres --port 1234 "$S3_BUCKET" "$SERVER_NAME" || {
     send_log "$HEALTHCHECK_URL/fail"
     exit 1
 }
 
-# zip backup
-zip -9 "$ZIP_FILE" "$BACKUP_FILE" || {
+# Delete old backups according to retention policy
+barman-cloud-backup-delete --cloud-provider aws-s3 --retention-policy "$RETENTION_POLICY" "$S3_BUCKET" "$SERVER_NAME" || {
     send_log "$HEALTHCHECK_URL/fail"
     exit 1
 }
 
-# Delete all but the most recent $RETAIN_BACKUPS_LOCAL backups and logs in BACKUP_DIR
-find "$BACKUP_DIR" -type f -name 'tsdb_backup_*.sql' | sort | head -n -"$RETAIN_BACKUPS_LOCAL" | xargs rm -f
-find "$BACKUP_DIR" -type f -name 'tsdb_backup_*.zip' | sort | head -n -"$RETAIN_BACKUPS_LOCAL" | xargs rm -f
-find "$BACKUP_DIR" -type f -name 'tsdb_backup_log_*.txt' | sort | head -n -"$RETAIN_BACKUPS_LOCAL" | xargs rm -f
-
-# Upload to AWS S3
-aws s3 cp "$ZIP_FILE" "$S3_BUCKET" --no-progress || {
-    send_log "$HEALTHCHECK_URL/fail"
-    exit 1
-}
-
-# Manage Retention
-aws s3 ls "$S3_BUCKET" | sort | head -n -"$RETAIN_BACKUPS_AWS" | awk '{print $4}' | xargs -I {} aws s3 rm "$S3_BUCKET"/{} || {
-    send_log "$HEALTHCHECK_URL/fail"
-    exit 1
-}
-
-# Notify healthchecks.io and send log
+# Notify healthchecks.io of success and send log
 send_log "$HEALTHCHECK_URL"
+
+# Finally, delete old log files in BACKUP_DIR
+find "$BACKUP_DIR" -type f -name 'barman_backup_log_*.txt' -mtime +$RETAIN_LOG_DAYS -exec rm -f {} \;
